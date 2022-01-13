4f57ad902ac8dbddfe721492aa117591ce7fb62b
okogeya <yusannagas@gmail.com>
2022-01-13 13:33:55 +0900

    新規投稿ページへの遷移エラーを修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-before_action :move_to_index, except: [:index, :show]
+before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @prototypes = Prototype.all
